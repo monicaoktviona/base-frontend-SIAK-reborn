238de051c665de13488d56ae6500291d99b2c577
monicaoktviona <monicaoktaviona2002@gmail.com>
2025-03-08 22:14:09 +0700

    universitas-bandung: buatAkunAlumni

--- components/FormFormBuatAkunAlumni.jsx
+++ components/FormFormBuatAkunAlumni.jsx
@@ -1,11 +1,11 @@
 /*
-	Generated on 22/10/2024 by UI Generator PRICES-IDE
+	Generated on 12/02/2025 by UI Generator PRICES-IDE
 	https://amanah.cs.ui.ac.id/research/ifml-regen
-	version 3.5.5
+	version 3.5.14
 */
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router";
 import {
   Button,
   Form,
@@ -13,6 +13,7 @@ import {
   MultiSelectionField,
   InputField,
   MultiSelectField,
+  RadioInputField,
   TextAreaField,
   RichTextField,
   VisualizationAttr,
@@ -70,6 +71,7 @@ const FormFormBuatAkunAlumni = ({
 		  formFields={[
 			  
 			  <Controller
+			    key="email"
 		        name="email"
 		        control={control}
 		        render={({ field, fieldState }) => (
@@ -87,7 +89,7 @@ const FormFormBuatAkunAlumni = ({
 		  ]}
 	
 		  itemsEvents={[
-				<Button type="submit" variant="primary">Create</Button>
+				<Button key="Create" type="submit" variant="primary">Create</Button>
 	    ]}
 	  />
   )
--- containers/DetailAkunAlumniPage.jsx
+++ containers/DetailAkunAlumniPage.jsx
@@ -1,25 +1,26 @@
 /*
-	Generated on 22/10/2024 by UI Generator PRICES-IDE
+	Generated on 12/02/2025 by UI Generator PRICES-IDE
 	https://amanah.cs.ui.ac.id/research/ifml-regen
-	version 3.5.5
+	version 3.5.14
 */
 import React, { useEffect, useState, useContext} from 'react'
 import { Button, Spinner } from "@/commons/components"
 import * as Layouts from '@/commons/layouts';
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams } from "react-router";
 import { HeaderContext } from "@/commons/components"
 
 import DetailsAkunAlumni from '../components/DetailsAkunAlumni'
 import getAkunAlumniDataBinding from '../services/getAkunAlumniDataBinding'
 const DetailAkunAlumniPage = props => {
-const [isLoading, setIsLoading] = useState({
+const { email } = useParams()
+
+	const [isLoading, setIsLoading] = useState({
 	detailsAkunAlumni: false,
 
 	});
 	const { setTitle } = useContext(HeaderContext);
 
 const [akunAlumniDataBinding, setAkunAlumniDataBinding] = useState()
-const { email } = useParams()
 useEffect(() => {
 	const fetchData = async () => {
 		try {

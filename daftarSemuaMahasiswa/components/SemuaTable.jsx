f13585dbaad037820bdb3c34deb74a6983b89a37
monicaoktviona <monicaoktaviona2002@gmail.com>
2025-03-09 08:44:30 +0700

    generated 1-by-1: daftar alumni-mahasiswa-industri

---
+++ daftarSemuaMahasiswa/components/SemuaTable.jsx
@@ -0,0 +1,86 @@
+/*
+	Generated on 22/10/2024 by UI Generator PRICES-IDE
+	https://amanah.cs.ui.ac.id/research/ifml-regen
+	version 3.5.10
+*/
+import React from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+import { useAuth } from '@/commons/auth';
+import { Button, Modal } from '@/commons/components';
+import { isMobile } from '@/commons/utils/responsive';
+
+import * as Layouts from "@/commons/layouts";
+
+const SemuaTable = ({ mahasiswaDataList, angkatanSelectionField
+
+	}) => {
+  const { checkPermission } = useAuth();
+  const navigate = useNavigate();
+  const detail = async (semuaItem) => {
+    isMobile() && navigate(`/daftarsemuamahasiswa/detail/${semuaItem.id}`
+    );
+  };
+  
+  
+  return (
+    <Layouts.ListComponentTableLayout
+  	  items={[mahasiswaDataList, angkatanSelectionField]}
+  	  detail={detail}
+  	  isSearchable
+  	  filterFields={[
+  	    {
+  	      label: "Angkatan",
+  	      featureName: "tahunAngkatan",
+  	      options: angkatanSelectionField,
+  	    }
+  	  ]}
+  	  itemsAttrs={[
+  		{
+            id: "nama",
+            condition: "isHeading",
+            label: "Nama",
+            featureName: "nama",
+  		}
+  ,
+  		{
+            id: "nPM",
+            condition: "isHeading",
+            label: "NPM",
+            featureName: "npm",
+  		}
+  ,
+  		{
+            id: "tahunAngkatan",
+            condition: "isHeading",
+            label: "Tahun Angkatan",
+            featureName: "tahunAngkatan",
+  		}
+  ,
+  		{
+            id: "statusAkademik",
+            condition: "isHeading",
+            label: "Status Akademik",
+            featureName: "statusAkademik",
+  		}
+  ,
+  	  ]}
+        itemsEvents={(semuaItem) => [
+          <Link to={`/daftarsemuamahasiswa/detail/${semuaItem.id}`}>
+            <Button 
+          	variant=
+          		"primary"
+            >
+              Detail
+            </Button>
+          </Link>
+          
+          
+        ]}
+    	  itemsModals={(semuaItem) => [
+        ]}
+  	/>
+  )
+};
+
+export default SemuaTable;
